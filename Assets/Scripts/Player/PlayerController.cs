702a62ed4a5ecbb24483d70f9e513abdfb684c31
ClemiltonLobo <clemilton.lobo@gmail.com>
2023-05-18 11:46:21 -0300

    Add new scripts to project

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,19 +4,55 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //publics
     [Header("Lerp")]
     public Transform target;
     public float lerpSpeed = 1f;
-
     public float speed = 1f;
+    public string tagToCheckEnemy = "Enemy";
+    public string tagToCheckEndLine = "EndLine";
+    public GameObject endScreen;
 
+
+    //privates
+    private Vector3 _pos;
+    private bool _canRun;
+
+    
     void Update()
     {
-        var pos = target.position;
-        pos.y = transform.position.y;
-        pos.x = transform.position.x;
+        if(!_canRun) return;
+        _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
 
-        transform.position = Vector3.Lerp(transform.position, pos, lerpSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
         transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == tagToCheckEnemy)
+        {
+            EndGame();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == tagToCheckEndLine)
+        {
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+        _canRun = false;
+        endScreen.SetActive(true);
+    }
+
+    public void StartToRun()
+    {
+        _canRun=true;
     }
 }
