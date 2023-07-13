f700595c5e687699c32ad9744522f39135d0eb23
ClemiltonLobo <clemilton.lobo@gmail.com>
2023-07-13 11:50:27 -0300

    Create LineRenderer

---
+++ Assets/Scripts/Utils/LineRendererHelper.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererHelper : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public List<Transform> positions;
+
+    public void Start()
+    {
+        lineRenderer.positionCount = positions.Count;
+    }
+
+    public void Update()
+    {
+        for (int i = 0; i < positions.Count; i++)
+        {
+            lineRenderer.SetPosition(i, positions[i].position);
+        }
+    }
+}
